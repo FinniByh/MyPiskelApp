507fb96c70d7626032f3fca879c5c2fc752bbf5d
finnibyh <gnomdanik@gmail.com>
2019-06-30 14:02:34 +0300

    initial project

---
+++ precache-manifest.cc94596c9fee80c669ab6fafa429ee6f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ec2c67370f1f5434b7ae5e383841e5",
+    "url": "/piskel/index.html"
+  },
+  {
+    "revision": "f1e2b7661c54763229be",
+    "url": "/piskel/static/css/main.7ab2f7e8.chunk.css"
+  },
+  {
+    "revision": "83d24e594dd44ea38e1e",
+    "url": "/piskel/static/js/2.2b1b50e5.chunk.js"
+  },
+  {
+    "revision": "f1e2b7661c54763229be",
+    "url": "/piskel/static/js/main.10f5c5d6.chunk.js"
+  },
+  {
+    "revision": "61a9eeb1571069e30aad",
+    "url": "/piskel/static/js/runtime~main.ed800120.js"
+  },
+  {
+    "revision": "5fb63cf9563eddda39ff36c58e661cd9",
+    "url": "/piskel/static/media/Screen.5fb63cf9.png"
+  },
+  {
+    "revision": "f20fd2da334b03f635c21510c7e12baa",
+    "url": "/piskel/static/media/bucket.f20fd2da.png"
+  },
+  {
+    "revision": "b0255afdba6359c2dfaf82db2ae2cedd",
+    "url": "/piskel/static/media/canvasBackground.b0255afd.jpg"
+  },
+  {
+    "revision": "e1ccf5a195cf14293913f5635bfa24ae",
+    "url": "/piskel/static/media/delete.e1ccf5a1.png"
+  },
+  {
+    "revision": "e9f04cb4ed29e522807d06048daeb4c5",
+    "url": "/piskel/static/media/eraser.e9f04cb4.png"
+  },
+  {
+    "revision": "b7a86a56dfc1c4546f3eec5390fa31e4",
+    "url": "/piskel/static/media/pen.b7a86a56.png"
+  }
+]);
